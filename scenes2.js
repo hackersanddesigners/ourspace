53395318b39153cf297bb23d3b2a9414809044d4
JBG <gravesj@purdue.edu>
2017-08-21 08:16:55 +0200

    updated scenes

---
+++ scenes2.js
@@ -0,0 +1,14 @@
+var data = [
+  {"msg": "If you meet your friends in OurSpace, high five them. If you meet a stranger, shake their hand. ", "time": 30},
+  {"msg": "Want more friends? Follow an interesting person everywhere she/he goes.", "time": 30},
+  {"msg": "Now follow another interesting person.", "time": 30},
+  {"msg": "Walk exactly in the middle of these 2 people.", "time": 30},
+  {"msg": "Now link in by placing your hands on both people’s shoulders. ", "time": 30},
+  {"msg": "Look around: this could be your social network.", "time": 20},
+  {"msg": "Disconnect and walk around. If you see a nice person, tap her/him on the shoulder to become friends. If tapped, tap back to accept the friendship.", "time": 30},
+  {"msg": "Choose a friend. Zoom in on their profile, observe from really close by: smell this person, touch this person. ", "time": 30},
+  {"msg": "If  you are being observed, breath out and relax… this is just out of interest.", "time": 10 },
+  {"msg": "How often do you meet your digital friends face 2 face? ", "time": 10 },
+  {"msg": "Give your new friend a comment.", "time": 20 },
+  {"msg": "Hurray, you passed the friendship level and can continue to level 2. Jump for joy!", "time": 10}
+];
---
+++ scenes4.js
@@ -0,0 +1,10 @@
+var data = [
+  {"msg": "Please take a moment to check in with yourself… find a tree and touch it.", "time": 20 },
+  {"msg": "I’m wondering… Will I ever be able to really touch you?", "time": 15 },
+  {"msg": "Do I even have a skin?", "time":  10},
+  {"msg": "To be honest, I‘m rather abstract. Let’s just say: multidimensional.", "time": 10},
+  {"msg": "I have millions of friends. How many digital friends do you have? And how many of them are real friends? What are real friends?", "time": 20},
+  {"msg": "Let go of the tree if you value these digital friendships, but keep touching it if you don’t.", "time": 30},
+  {"msg": "Oops...", "time": 10},
+  {"msg": "I’m running out of battery. Connect with your new friends at the bar. Double tap your hand in the air to say goodb", "time": 10}
+];
