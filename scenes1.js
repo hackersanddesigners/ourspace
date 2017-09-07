8145b43df9a7f98eb403a6ccce3b513d49361340
JBG <gravesj@purdue.edu>
2017-09-07 20:12:37 +0200

    new scenes

--- scenes1.js
+++ scenes1.js
@@ -1,6 +1,8 @@
 var data = [
-  {"msg": "Hello, I’m your new digital friend. You will find out more about me later, but first I will navigate you through Ourspace.", "time": 20},
-  {"msg": "Put your hand in the air and double tap to start browsing Ourspace! Go! Browse Ourspace by walking around. Keep an eye on your phone. Stop only when you get my message.", "time": 20},
+  {"msg": "Hello, I’m your new digital friend.", "time": 10},
+  {"msg": "You will find out more about me later, but first I will navigate you through Ourspace.", "time": 10},
+  {"msg": "Put your hand in the air and double tap to start browsing Ourspace! Go!", "time": 10},
+  {"msg": "Browse Ourspace by walking around. Keep an eye on your phone. Stop only when you get my message.", "time": 20},
   {"msg": "Walk in straight lines until you hit an obstacle, then make a quarter turn and repeat this pattern.", "time": 30},
   {"msg": "The first thing to do in a new social space, is to make new connections. To get attention, it’s important to act happy and confident. Take big steps, straighten your back, widen your shoulders and smile. ", "time": 30},
   {"msg": "Congratulations, you passed the exploration level and can continue to level 1. Browse on!", "time": 10}
--- scenes4.js
+++ scenes4.js
@@ -1,10 +1,12 @@
 var data = [
-  {"msg": "Please take a moment to check in with yourself… find a tree and touch it.", "time": 20 },
+  {"msg": "Please take a moment to check in with yourself… sit down, on the ground.", "time": 20 },
   {"msg": "I’m wondering… Will I ever be able to really touch you?", "time": 15 },
   {"msg": "Do I even have a skin?", "time":  10},
   {"msg": "To be honest, I‘m rather abstract. Let’s just say: multidimensional.", "time": 10},
-  {"msg": "I have millions of friends. How many digital friends do you have? And how many of them are real friends? What are real friends?", "time": 20},
-  {"msg": "Let go of the tree if you value these digital friendships, but keep touching it if you don’t.", "time": 30},
+  {"msg": "I have millions of friends. How many digital friends do you have?", "time": 10},
+  {"msg": "And how many of them are real friends? What are real friends?", "time": 10},
+  {"msg": "Lie down on the floor if you value these friendships, but keep sitting down if you don’t", "time": 30},
+  {"msg": "If you sit down, jump up and shout!", "time": 30},
   {"msg": "Oops...", "time": 10},
   {"msg": "I’m running out of battery. Connect with your new friends at the bar. Double tap your hand in the air to say goodb", "time": 10}
 ];
